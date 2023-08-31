2062c63a8e7970bbca6f7066dd5003040ecc4c58
EC2 Default User <ec2-user@ip-172-31-77-100.ec2.internal>
2023-08-31 20:27:28 +0000

    commit last

---
+++ terraform-file/variable.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "Slhttnunlu"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "xxxx"
+}
+
+variable "hosted-zone" {
+  default = "slhttnunlu.net"
+}
